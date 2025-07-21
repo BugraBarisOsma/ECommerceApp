120c60fd80f4681c79a90d30bdd48da791fea959
BugraBarisOsma <bugrabarisosma@gmail.com>
2025-07-21 18:05:33 +0300

    initial commit

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegisteration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceAPI.Application.Abstractions;
+using ECommerceAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerceAPI.Persistence
+{
+    // Abstractionu var olan IoC'ye eklemek icin bu extension methodu ekledik (nuget de eklendi buraya). Program.cs'deki services ile ayni kutuphane. Bu Extensionu yazdiktan sonra bunu program.cs icerisinde cagirilmasi lazim 
+    public static class ServiceRegisteration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services) 
+        {
+            services.AddSingleton<IProductService,ProductService>();
+        }
+    }
+}
